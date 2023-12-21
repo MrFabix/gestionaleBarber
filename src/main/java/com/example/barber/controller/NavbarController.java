4126ae5706da27f802fc0c0bc469f8aefc2fa8c8
Fabio Massimo <fmaxprr@gmail.com>
2023-12-21 18:48:22 +0100

    #1 prova commit

---
+++ src/main/java/com/example/barber/controller/NavbarController.java
@@ -0,0 +1,10 @@
+package com.example.barber.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class NavbarController {
+    @FXML
+    protected Button homeButton;
+
+}
