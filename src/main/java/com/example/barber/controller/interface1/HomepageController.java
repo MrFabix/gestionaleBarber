b24baaafb68dcdfbc916899fec198c24e07ed369
Fabio Massimo <fmaxprr@gmail.com>
2024-01-08 15:51:05 +0100

    cambio dipendenze e ristrutturazione progetto

--- src/main/java/com/example/barber/controller/interface1/HomepageController.java
+++ src/main/java/com/example/barber/controller/interface1/HomepageController.java
@@ -1,4 +1,4 @@
-package com.example.barber.controller;
+package com.example.barber.controller.interface1;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,6 @@ module com.example.barber {
 
     opens com.example.barber to javafx.fxml;
     exports com.example.barber;
-    exports com.example.barber.controller;
-    opens com.example.barber.controller to javafx.fxml;
+    exports com.example.barber.controller.interface1;
+    opens com.example.barber.controller.interface1 to javafx.fxml;
 }
